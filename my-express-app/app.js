352dfda84c69cbd1b00b2effe383aad43127f720
Gerald Calotes <gerald.calotes.dev@gmail.com>
2024-11-18 10:29:45 -0700

    Added search query in app.js

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -5,6 +5,12 @@ app.get('/user/:id',(req,res) => {
 	res.send(`User id is ${userId}`);
 });
 
+app.use('/search',(req,res) => {
+	const query = req.query.q;
+	res.send(`The query search term is ${query}`);
+});
+
+
 const port=3000;
 app.listen(port,() => {
 	console.log('Server listening to port 3000');
